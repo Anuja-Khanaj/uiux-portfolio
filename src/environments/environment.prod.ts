5739370c00e8277d14989d062c186d9579c33961
Anuja Khanaj <anujahkhanaj2003@gmail.com>
2024-06-07 09:36:48 +0530

    environment added

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment ={
+    production:false ,
+    firebaseConfig : {
+      apiKey: "AIzaSyB5Kx5UhNV2M63Edsrqc87X39nD-xOSpRQ",
+      authDomain: "ang-blog-1e504.firebaseapp.com",
+      databaseURL: "https://ang-blog-1e504-default-rtdb.firebaseio.com",
+      projectId: "ang-blog-1e504",
+      storageBucket: "ang-blog-1e504.appspot.com",
+      messagingSenderId: "589302296220",
+      appId: "1:589302296220:web:a834df2c1f8ce38003c759"
+    }
+};
